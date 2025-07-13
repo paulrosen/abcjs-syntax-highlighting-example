7d57534d8dd7718f22ebac3973600a36311ca8d9
paulrosen <junk@paulrosen.net>
2025-07-13 12:38:05 -0400

    clean up

---
+++ src/abc-string.ts
@@ -0,0 +1,33 @@
+export const abcString = `X:1
+M:4/4
+L:1/16
+%%titlefont Times 22.0
+%%partsfont box
+%%barnumbers 1
+T: Variety of element types
+T:This is a subtitle
+C: public domain
+R: Hit it
+A: Yours Truly
+S: My own testing
+W: Now is the time for all good men
+W:
+W: To come to the aid of their party.
+H: This shows every type of thing that can possibly be drawn.
+H:
+H: And two lines of history!
+Q: "Easy Swing" 1/4=140
+P: AABB
+%%staves {(PianoRightHand extra) (PianoLeftHand)}
+V:PianoRightHand clef=treble+8 name=RH
+V:PianoLeftHand clef=bass name=LH
+K:Bb%comment
+P:A
+%%text there is some random text
+%%sep 0.4cm 0.4cm 6cm
+[V: PianoRightHand] !mp![b8B8d8] f3g !//!f4|!<(![d12b12] !<)![b4g4]|z4  b^f_df (3B2d2c2 B4|1[Q:"left" 1/4=170"right"]!f![c4f4] z4 [b8d8]| (3[G8e8] Tu[c8f8] G8|]
+w:Strang- ers % comment on lyric line
+% second line
+[V: extra] B,16 | "Bb"{C}B,4 ({^CD}B,4 =B,8) | % comment on music line
+T:Inserted subtitle
+[V: PianoLeftHand] B,6 .D2 !arpeggio![F,8F8A,8]|(B,2 B,,2 C,12)|"^annotation"F,16 & B B, B' B,2 _b' ^^B=B,|[F,16D,16]|Z2|]`
